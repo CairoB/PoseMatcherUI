13ca42f1127242e6c582be1541ce0553f424b35d
Cairo Batt <cairo.batt@gmail.com>
2023-10-25 17:17:47 -0600

    BrowserRouter --> HashRouter
    BrowserRouter apparently not supported by gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import AboutPage from "./components/pages/AboutPage";
 import NoMatch from "./components/pages/NoMatch";
@@ -7,14 +7,14 @@ import NoMatch from "./components/pages/NoMatch";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Navigate replace to="/home" />} />
           <Route index path="/home" element={<HomePage></HomePage>}></Route>
           <Route path="/about" element={<AboutPage></AboutPage>}></Route>
           <Route path="*" element={<NoMatch></NoMatch>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
